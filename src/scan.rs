23c4e2ec251b12a3874d19afc0224020860a4f73
gyara <me@gyara.moe>
2020-11-29 12:37:52 +0900

    use XDG data dir

--- src/scan.rs
+++ src/scan.rs
@@ -1,10 +1,22 @@
 use crate::error::{Error, Result};
-use std::{collections::HashSet, fs, path::Path};
+use std::{collections::HashSet, env::current_dir, fs, path};
 use walkdir::{DirEntry, WalkDir};
 
-static FILE_LIST_CACHE: &str = ".strikeout_cache";
+const APPLICATION: &str = "strikeout";
 
-pub fn scan_new_file(dir: &Path, file_list: &mut HashSet<String>) -> Vec<DirEntry> {
+fn data_file() -> path::PathBuf {
+    let cur = current_dir().expect("can not parse current dir");
+    let cur = cur.to_string_lossy().to_string();
+    let name = cur.replace(path::MAIN_SEPARATOR, "_");
+    let mut data_dir = dirs::data_dir().expect("system error");
+    data_dir.push(APPLICATION);
+    // set dir
+    fs::create_dir_all(&data_dir).ok();
+    data_dir.push(name);
+    data_dir
+}
+
+pub fn scan_new_file(dir: &path::Path, file_list: &mut HashSet<String>) -> Vec<DirEntry> {
     let mut new_file_list = Vec::new();
     for entry in WalkDir::new(dir).into_iter().filter_entry(|e| !is_hidden(e)) {
         if let Ok(entry) = entry {
@@ -35,23 +47,20 @@ fn is_hidden(entry: &DirEntry) -> bool {
 }
 
 pub fn get_file_list() -> Result<HashSet<String>> {
-    let file = fs::File::open(FILE_LIST_CACHE)?;
+    let file = fs::File::open(data_file())?;
     let file_list = serde_json::from_reader(file)?;
     Ok(file_list)
 }
 
 pub fn store_file_list(old_file_list: &HashSet<String>) -> Result<()> {
-    let file = fs::File::with_options()
-        .write(true)
-        .create(true)
-        .open(FILE_LIST_CACHE)?;
+    let file = fs::File::with_options().write(true).create(true).open(data_file())?;
     serde_json::to_writer(file, old_file_list)?;
     Ok(())
     // pass
 }
 
 pub fn clear_cache() -> Result<()> {
-    let file = fs::File::open(FILE_LIST_CACHE)?;
+    let file = fs::File::open(data_file())?;
     file.set_len(0)?;
     Ok(())
 }
